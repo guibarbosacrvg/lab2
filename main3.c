7e6ba402d22cfd4359292600d70077076c59e44a
Guilherme <guibarbosa@aluno.puc-rio.br>
2023-09-25 21:47:38 -0300

    I

--- main3.c
+++ main3.c
@@ -11,21 +11,22 @@
 #define V_SIZE 10000
 #define NUM_PROC 2
 
-int main (int argc, char *argv[]){
-    int segmento, segmento2, segmento_time, id, pid, status;
-    int *p, *p2;
-    clock_t *p_time;
+int main(int argc, char *argv[]) {
+    int segmento, id, pid, status;
+    int *p;
     segmento = shmget(IPC_PRIVATE, sizeof(int) * V_SIZE, 0666);
-    p = (int *) shmat(segmento,0 ,0);
-    for(int i = 0; i < V_SIZE; i++){
+    p = (int *)shmat(segmento, 0, 0);
+    
+    // Inicialize o vetor com o valor 5
+    for (int i = 0; i < V_SIZE; i++) {
         p[i] = 5;
     }
-    
+
     int i = 0;
-    while(i < NUM_PROC){
+    while (i < NUM_PROC) {
         pid = fork();
-        if(pid == 0){
-            for(int j = V_SIZE/NUM_PROC * i; j < V_SIZE/NUM_PROC * (i + 1); j++){
+        if (pid == 0) {
+            for (int j = V_SIZE / NUM_PROC * i; j < V_SIZE / NUM_PROC * (i + 1); j++) {
                 p[j] *= 2;
                 p[j] += 2;
             }
@@ -33,9 +34,26 @@ int main (int argc, char *argv[]){
         }
         i++;
     }
-   
-    for(int i = 0; i < V_SIZE; i++){
-        printf("%d ", p[i]);
+
+    // Aguarde a conclusão dos trabalhadores
+    for (int i = 0; i < NUM_PROC; i++) {
+        wait(NULL);
     }
+
+    // Verifique se todas as posições têm valores iguais
+    int areEqual = 1; // Supomos que todos são iguais
+    for (int i = 1; i < V_SIZE; i++) {
+        if (p[i] != p[0]) {
+            areEqual = 0; // Pelo menos uma posição é diferente
+            break;
+        }
+    }
+
+    if (areEqual) {
+        printf("Todas as posições têm valores iguais: %d\n", p[0]);
+    } else {
+        printf("Algumas posições têm valores diferentes.\n");
+    }
+
     return 0;
 }
